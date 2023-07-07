46a668e84008d3cb3f92bcb103c182846c5b8e21
lesobrod <lesobrod@yandex.ru>
2023-07-07 22:00:51 +0300

    Create TestLengthEF.jl

---
+++ TestLengthEF.jl
@@ -0,0 +1,10 @@
+function checkLEF(f::Rational{BigInt}, k::Int)
+  # Return true, if for f < 1 there is egyptian fraction of length k
+  # Otherwise false
+  isone(numerator(f)) && return true
+  isone(k) && return false
+  for i ∈ ceil(BigInt, inv(f)):floor(BigInt, k / f)
+    check(f - (true // i), k - 1) && return true
+  end
+  false
+end
