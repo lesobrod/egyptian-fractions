fd455d7c4cbb04a571f0ba45255b6a7d158a05b9
lesobrod <lesobrod@yandex.ru>
2023-07-08 19:01:57 +0300

    Update FindLSEF.jl

--- FindLSEF.jl
+++ FindLSEF.jl
@@ -1,4 +1,14 @@
-using TestLengthEF
+function checkLEF(f::Rational{BigInt}, k::Int)::Bool
+  # Return true, if for f < 1 there is egyptian fraction of length k
+  # Otherwise false
+  isone(numerator(f)) && return true
+  isone(k) && return false
+  for i ∈ ceil(BigInt, inv(f)):floor(BigInt, k / f)
+    check(f - (true // i), k - 1) && return true
+  end
+  false
+end
+
 function lenSEF(f::Rational{BigInt})
   k = 1
   while checkLEF(f, k)::Bool
