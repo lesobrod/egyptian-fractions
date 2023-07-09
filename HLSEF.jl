433c523e6d78d21ba737ad9bf5c134d8718d51b3
lesobrod <lesobrod@yandex.ru>
2023-07-09 19:15:23 +0300

    Update HLSEF.jl

--- HLSEF.jl
+++ HLSEF.jl
@@ -9,7 +9,7 @@ function scanHLSEF(mn, mx)
   for j = mn:mx
     h = harmNum(j)
     res = min(LSEF(h) - floor(BigInt, h), LSEF(ceil(BigInt, h) - h))
-    println("$(j)-th harmonic number needs $(res) egyptian fraction to reach integer")
+    println("$(j)-th harmonic number needs $(res) egyptian fractions to reach integer")
   end
 end
 
