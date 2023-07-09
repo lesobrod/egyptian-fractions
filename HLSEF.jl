0f7b38f5e4256cac38e8ad1974d974e5703edd24
lesobrod <lesobrod@yandex.ru>
2023-07-09 19:10:21 +0300

    Update HLSEF.jl

--- HLSEF.jl
+++ HLSEF.jl
@@ -1,7 +1,16 @@
 include("FindLSEF.jl")
 
 function harmNum(n)
+  # Return n-th harmonic number as Rational BigInt
   mapreduce(i->Rational{BigInt}(1,i), +, [1:n;])
 end
 
+function scanHLSEF(mn, mx)
+	for j = mn:mx
+    h = harmNum(j)
+    res = min(LSEF(h) - floor(BigInt, h), LSEF(ceil(BigInt, h) - h))
+    println("$(j)-th harmonic number needs $(LSEF(j)) egyptian fraction to reach integer")
+	end
+end
 
+scanHLSEF(2, 10)
