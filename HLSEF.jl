09655c2643775ad4bd79cdea2ab682df3b1c789f
lesobrod <lesobrod@yandex.ru>
2023-07-11 07:50:16 +0300

    Update HLSEF.jl

--- HLSEF.jl
+++ HLSEF.jl
@@ -1,9 +1,4 @@
-include("FindLSEF.jl")
-
-function harmNum(n)
-  # Return n-th harmonic number as Rational BigInt
-  mapreduce(i->Rational{BigInt}(1,i), +, [1:n;])
-end
+include("LSEF.jl")
 
 function scanHLSEF(mn, mx)
   for j = mn:mx
