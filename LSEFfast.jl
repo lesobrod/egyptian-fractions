c01b3c619735533b0d4df4f4574c357aa212673d
lesobrod <lesobrod@yandex.ru>
2023-07-16 10:27:02 +0300

    Update and rename LSEF.jl to LSEFfast.jl

--- LSEFfast.jl
+++ LSEFfast.jl
@@ -1,20 +1,22 @@
-function checkLEF(f::Rational{BigInt}, k::Int)::Bool
+using FastRationals
+
+function checkLEF(f::FastRational{BigInt}, k::Int)::Bool
   # Return true, if for f < 1 there is egyptian fraction of length k
   # Otherwise false
   isone(numerator(f)) && return true
   isone(k) && return false
-  for i ∈ ceil(BigInt, inv(f)):floor(BigInt, k / f)
-    checkLEF(f - (true // i), k - 1) && return true
+  for i in ceil(BigInt, inv(f)):floor(BigInt, k / f)
+    checkLEF(FastRational{BigInt}(f - inv(i)), k - 1) && return true
   end
   false
 end
 
 function harmNum(n)
   # Return n-th harmonic number as Rational BigInt
-  mapreduce(i->Rational{BigInt}(1,i), +, [1:n;])
+  mapreduce(i->FastRational{BigInt}(1,i), +, [1:n;])
 end
 
-function LSEF(f::Rational{BigInt})::Int
+function LSEF(f::FastRational{BigInt})::Int
   # Return the minimal possible length of egyptian fraction for f
   k = 1
   while !checkLEF(f, k)
